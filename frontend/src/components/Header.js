733994308d0fc95ee720c073c8781828dee01668
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-07-16 15:08:35 +0900

    履歴部分のAPI作成

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import {useHistory, BrowserRouter as Router} from 'react-router-dom'
+import Axios from 'axios';
 
 const Header = () =>{
     const history = useHistory();
     const handleLink = path => history.push(path);
+
+    const toOrderHistory = ()=>{
+        Axios.get('http://127.0.0.1:5000/order_history')
+        .then(function(res){
+            console.log(res)
+        })
+        handleLink('/order_history')
+    }
     return (
         <React.Fragment>
             <Router>
             <button onClick={() => handleLink('/')}>Home</button>
             <button onClick={() => handleLink('/cart')}>カート</button>
             <button onClick={() => handleLink('/item_detail')}>商品詳細</button>
-            <button onClick={() => handleLink('/order_history')}>履歴</button>
+            <button onClick={() => toOrderHistory()}>注文履歴</button>
+            {/* <button onClick={() => handleLink('/order_history')}>履歴</button> */}
             <button onClick={() => handleLink('/complete')}>完了画面</button>
             <button onClick={()=>handleLink('/')}>トップへ戻る</button>
             </Router>
