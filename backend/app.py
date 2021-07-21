33790b3da8727d9a9926ea0a3d0b9c40c84461cd
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-07-21 11:54:51 +0900

    home画面修正

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import relationship
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 CORS(app, support_credentials=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mwmw1225zwzw@localhost:5432/fr_ec'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:*******@localhost:5432/****'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = False
 
@@ -29,7 +29,7 @@ def get_connection():
     port = '5432'
     users = 'postgres'
     dbnames = 'fr_ec'
-    passwords = 'mwmw1225zwzw'
+    passwords = '*****'
     return psycopg2.connect("host=" + localhost + " port=" + port + " user=" + users + " dbname=" + dbnames + " password=" + passwords)
 
 class Item(db.Model):
